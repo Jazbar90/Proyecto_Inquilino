8113279d1ef59c55faa8d810d3c9f7c71e455f93
jazbar90 <jazbarc90@gmail.com>
2021-10-23 23:55:06 -0600

    Agregar archivos de proyecto.

---
+++ Database/dbo/StoreProcedures/InquilinoActualizar.sql
@@ -0,0 +1,34 @@
+﻿CREATE PROCEDURE [dbo].[InquilinoActualizar]
+	@Id_TipoInquilino int,
+	@Descripcion VARCHAR(250),
+	@Estado BIT
+
+AS BEGIN
+SET NOCOUNT ON
+
+	BEGIN TRANSACTION TRANS
+
+		BEGIN TRY
+
+		UPDATE dbo.TipoInquilino SET
+			Descripcion = @Descripcion,
+			Estado = @Estado
+		WHERE
+			Id_TipoInquilino = @Id_TipoInquilino
+		
+	COMMIT TRANSACTION TRANS
+	SELECT 0 AS CodeError, '' AS MsgError
+
+	END TRY
+
+	BEGIN CATCH
+
+	SELECT
+		ERROR_NUMBER() AS CodeError,
+		ERROR_MESSAGE() AS MsgError
+
+	ROLLBACK TRANSACTION TRANS
+
+	END CATCH
+
+END
---
+++ InitialData/ScriptConfig/Inquilino.sql
@@ -0,0 +1,36 @@
+﻿DROP TABLE IF EXISTS #InquilinoTemp
+
+SELECT
+Id_TipoInquilino, Descripcion, Estado INTO #InquilinoTemp
+FROM(
+VALUES
+(1, 'FISICO', ''),
+(2,	'JURIDICO', '')
+)AS TEMP (Id_TipoInquilino, Descripcion, Estado)
+
+-----ACTUALIZAR DATOS TABLA-----
+UPDATE I SET
+	I.Descripcion = TM.Descripcion,
+	I.Estado = TM.Estado
+FROM dbo.TipoInquilino I
+INNER JOIN #InquilinoTemp TM
+	ON I.Id_TipoInquilino = TM.Id_TipoInquilino
+
+----INSERTAR DATOS TABLA----
+
+SET IDENTITY_INSERT dbo.TipoInquilino ON
+
+INSERT INTO dbo.TipoInquilino (
+	Id_TipoInquilino,Descripcion,Estado)
+SELECT
+	Id_TipoInquilino,Descripcion,Estado 
+FROM #InquilinoTemp
+
+EXCEPT
+SELECT
+Id_TipoInquilino,Descripcion,Estado
+FROM dbo.TipoInquilino
+
+SET IDENTITY_INSERT dbo.TipoInquilino OFF
+
+GO
